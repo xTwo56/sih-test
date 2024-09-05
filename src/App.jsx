1649f17f95d1dbf7bdf74a8ff84a622fa35bf174
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2024-09-06 02:24:46 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import Home from './pages/home'
 import Signup from './pages/signup'
 import Login from './pages/login'
 
+import Listing from "./pages/listing"
+
 function App() {
 
   return (
     <div>
+   
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/listing" element={<Listing />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
